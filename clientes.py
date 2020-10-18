da48968026b5a794196acedbe44941c77c5369bd
daniellop199731 <42153908+daniellop199731@users.noreply.github.com>
2020-10-17 20:34:22 -0500

    Add files via upload

---
+++ clientes.py
@@ -0,0 +1,6 @@
+clientes = [
+    {"nombre":"Daniel", "id":"1040755872"},
+    {"nombre":"Santiago", "id":"1040755873"},
+    {"nombre":"Jessica", "id":"1040755874"},
+    {"nombre":"Juan", "id":"1040755875"}
+]
